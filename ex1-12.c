44600f1b8f352c3db8e4993ba8a350b05f7981df
David Alkire <dalkire@npr.org>
2015-03-03 19:57:33 -0500

    Exercise 12.

---
+++ ex1-12.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int c;
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\t')
+			c = '\n';
+		printf("%c", c);
+	}
+
+	printf("\n\n");
+
+	return 0;
+}
